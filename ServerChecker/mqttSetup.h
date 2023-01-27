0462db409b89fda8ac4c71989c595879d0215d8a
Johannes <80753737+myDevLog@users.noreply.github.com>
2023-01-27 14:47:20 +0100

    Added mqttSetup file

---
+++ ServerChecker/mqttSetup.h
@@ -0,0 +1,5 @@
+IPAddress server(127, 0, 0, 1);
+const uint16_t serverPort = 1883;
+const char* mqttClientId = "ESP8266Client-ServerChecker";
+const char* mqttClientUsername = "";
+const char* mqttClientPassword = "";
