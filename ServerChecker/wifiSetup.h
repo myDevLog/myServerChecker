7eae8800c3e86903ad395588e2a5bfa8596583a0
Johannes <80753737+myDevLog@users.noreply.github.com>
2023-01-27 14:49:21 +0100

    Added WiFiSetup File

---
+++ ServerChecker/wifiSetup.h
@@ -0,0 +1,5 @@
+const char* wifi_ssid = "eduroam";
+const char* wifi_identity = "";
+// Please put your full E-Mail Address of your organisational Network in as the Username
+const char* wifi_username = "";
+const char* wifi_password = "";
